54eac02e25997cc5853bea418875fbb53c36f470
abd1llaah <nurabdillah244@gmail.com>
2026-04-18 19:24:25 +0700

    Update app py

--- app.py
+++ app.py
@@ -3,20 +3,31 @@ import time
 
 st.title("Simulasi Lampu Lalu Lintas 🚦")
 
-# Data lampu: (nama, durasi, emoji)
 lampu = [
-    ("Merah", 5, "🔴"),
-    ("Hijau", 3, "🟢"),
-    ("Kuning", 2, "🟡")
+    ("Merah", 5),
+    ("Hijau", 3),
+    ("Kuning", 2)
 ]
 
 placeholder = st.empty()
 
-# Tombol start
+def tampil(warna_aktif, sisa):
+    merah = "🔴" if warna_aktif == "Merah" else "⚫"
+    kuning = "🟡" if warna_aktif == "Kuning" else "⚫"
+    hijau = "🟢" if warna_aktif == "Hijau" else "⚫"
+
+    placeholder.markdown(f"""
+    ## {merah}
+    ## {kuning}
+    ## {hijau}
+
+    ### Lampu: {warna_aktif}
+    ### Sisa waktu: {sisa} detik
+    """)
+
 if st.button("Start"):
-    for _ in range(3):  # jumlah putaran
-        for warna, durasi, emoji in lampu:
+    for _ in range(3):
+        for warna, durasi in lampu:
             for sisa in range(durasi, 0, -1):
-                placeholder.markdown(f"## {warna} {emoji}")
-                placeholder.write(f"Sisa waktu: {sisa} detik")
+                tampil(warna, sisa)
                 time.sleep(1)
